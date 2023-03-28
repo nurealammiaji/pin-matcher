fbce79de56a7dfbf724e1fd8726b7bed7a5fd431
nurealammiaji <nurealam.miaji@gmail.com>
2023-03-28 19:53:01 +0600

    pin generator function added

--- script/app.js
+++ script/app.js
@@ -1,6 +1,11 @@
 document.getElementById('btn-pin-generator').addEventListener('click', function(){
-    let randomPin = Math.round(Math.random()*10000);
-    let randomPinString = randomPin + '';
+    
+    function fourDigitPin() {
+        let randomPin = Math.round(Math.random()*10000);
+        let randomPinString = randomPin + '';
+        return randomPinString;
+    }
+
     if (randomPinString.length === 4){
         console.log(randomPinString);
     }
