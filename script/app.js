09599711bfbc073948fb5224783f51a37ca4d1ca
nurealammiaji <nurealam.miaji@gmail.com>
2023-03-30 12:53:05 +0600

    code re written

--- script/app.js
+++ script/app.js
@@ -1,13 +1,6 @@
 document.getElementById('btn-pin-generator').addEventListener('click', function(){
-    
-    function fourDigitPin() {
-        let randomPin = Math.round(Math.random()*10000);
-        let randomPinString = randomPin + '';
-        return randomPinString;
-    }
-
-    if (randomPinString.length === 4){
-        console.log(randomPinString);
-    }
-
+    let generatedPin = Math.random()*10000;
+    let generatedPinRound = Math.round(generatedPin);
+    let generatedPinString = generatedPinRound + '';
+    console.log(generatedPinString);
 })
