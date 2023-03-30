7a00d017f6d8ae0c15a0424b1289b10e1c36500e
nurealammiaji <nurealam.miaji@gmail.com>
2023-03-30 15:32:06 +0600

    function added

--- script/app.js
+++ script/app.js
@@ -1,6 +1,19 @@
+function fourDigitPin() {
+    let pin = pinGenerator();
+    let pinString = pin + '';
+    if (pinString.length === 4) {
+        return pin;
+    }
+    else {
+        fourDigitPin();
+    }
+}
+
+function pinGenerator() {
+    let generatedPin = Math.round(Math.random()*10000);
+    return generatedPin;
+}
+
 document.getElementById('btn-pin-generator').addEventListener('click', function(){
-    let generatedPin = Math.random()*10000;
-    let generatedPinRound = Math.round(generatedPin);
-    let generatedPinString = generatedPinRound + '';
-    console.log(generatedPinString);
+    let pinGen = fourDigitPin();
 })
